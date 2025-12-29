5f40b3d5ff6bb7130c19ad14598b96ca3e257776
Akilach01 <akilapeiris92@gmail.com>
2025-12-29 12:21:56 +0530

    added plan.ts

---
+++ src/services/plan.ts
@@ -0,0 +1,28 @@
+import api from "./api"
+
+
+export const createPlan = async(data:any)=>{
+    const res = await api.post("/plans", data);
+    return res.data;
+};
+
+export const getMyPlans = async()=>{
+    const res = await api.get("/plans/me");
+    return res.data;
+};
+
+export const getAllPlans = async()=>{
+    const res = await api.get("/plans");
+    return res.data;
+};
+
+export const approvePlan = async(id:string)=>{
+    const res = await api.patch(`/plans/${id}/approve`);
+    return res.data;
+};
+
+export const rejectPlan = async(id:string)=>{
+    const res = await api.put(`/plans/${id}/reject`);
+    return res.data;
+};
+
