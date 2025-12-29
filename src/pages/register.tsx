6286c9640cc867fdaa0c724ec1ad5f2020210cc5
Akilach01 <akilapeiris92@gmail.com>
2025-12-29 11:39:24 +0530

    updated register

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -48,6 +48,11 @@ export default function Register(){
             onChange={(e)=> setPassword(e.target.value)}
             />
 
+            <select value={role} onChange={(e) => setRole(e.target.value)}>
+          <option value="user">User</option>
+          <option value="admin">Admin</option>
+            </select>
+
             <button onClick={handleRegister}>Register</button>
 
         </div>
