ca7e4c93ae04847990016fbb0b3e24b1b874b139
Akilach01 <akilapeiris92@gmail.com>
2025-12-31 11:41:51 +0530

    added approve,reject for admin

---
+++ src/pages/admin/AdminPlans.tsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import { approvePlan, getAllPlans, rejectPlan } from "../../services/plan";
+
+
+export default function AdminPlans(){
+    const[plans, setPlans] = useState<any[]>([]);
+
+    const loadPlans = async()=>{
+        const res = await getAllPlans();
+        setPlans(res.plans);
+    };
+
+    useEffect(()=>{
+        loadPlans();
+    },[]);
+
+
+    return(
+        <div>
+            <h2>All Plans</h2>
+
+            {plans.map((p) => (
+          <div key={p._id} className="border p-2 mb-2">
+          <h3>{p.title}</h3>
+          <p>User: {p.user?.email}</p>
+          <p>Status: {p.status}</p>
+
+          {p.status === "pending" && (
+            <>
+            <button onClick={()=>approvePlan(p._id).then(loadPlans)}>Approve</button>
+             <button onClick={()=>rejectPlan(p._id).then(loadPlans)}>Reject</button>
+            </>
+          )}
+           </div>
+            ))}
+        </div>
+    );
+}
